0b8e571f21b80db67edd416f675e6fed729712cd
enzomoralesl <enzo_moralesl@yahoo.com.br>
2025-11-18 18:57:33 -0300

    usage of PrefixTree

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        PrefixTree trie = new PrefixTree();
+
+        trie.insert("Python");
+        trie.insert("PHP");
+        trie.insert("Golang");
+
+        trie.search("PHP");     // Return True
+        trie.search("Java");    // Return False
+        trie.search("Pyt");     // Return False
+
+        if (trie.search("Python"))      // Return True
+            trie.startsWith("Pyt");             // Return True
+        trie.startsWith("Javascript");      // Return False
+
+        trie.startsWith("Golang");                // Return True
+        trie.search("Golang");                // Return True
     }
 }
